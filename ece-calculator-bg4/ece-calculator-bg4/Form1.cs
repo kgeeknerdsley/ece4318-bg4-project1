07d33427f6d52310bfaac044ff3ceaa0577292a3
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2019-10-24 17:07:04 -0700

    Create form project

---
+++ ece-calculator-bg4/ece-calculator-bg4/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ece_calculator_bg4
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			FilterCalculator filterCalc = new FilterCalculator(); //all forms are classes, make a new instance
+			filterCalc.Show();//show the new form
+		}
+	}
+}
