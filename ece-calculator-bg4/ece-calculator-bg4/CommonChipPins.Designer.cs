80f78dc25c851e1430dd6a9eb70b2714785c6d0c
ggenie <51795989+ggenie@users.noreply.github.com>
2019-11-13 21:01:51 -0800

    Common Datasheets First Commit

--- ece-calculator-bg4/ece-calculator-bg4/CommonChipPins.Designer.cs
+++ ece-calculator-bg4/ece-calculator-bg4/CommonChipPins.Designer.cs
@@ -28,12 +28,157 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "CommonChipPins";
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.addClassBtn = new System.Windows.Forms.Button();
+            this.classText = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.addFileBtn = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.fileListBox = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.addClassBtn);
+            this.groupBox1.Controls.Add(this.classText);
+            this.groupBox1.Location = new System.Drawing.Point(78, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(427, 125);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add Class";
+            // 
+            // addClassBtn
+            // 
+            this.addClassBtn.Location = new System.Drawing.Point(297, 50);
+            this.addClassBtn.Name = "addClassBtn";
+            this.addClassBtn.Size = new System.Drawing.Size(124, 48);
+            this.addClassBtn.TabIndex = 1;
+            this.addClassBtn.Text = "Add";
+            this.addClassBtn.UseVisualStyleBackColor = true;
+            this.addClassBtn.Click += new System.EventHandler(this.addNewClassBtn);
+            // 
+            // classText
+            // 
+            this.classText.Location = new System.Drawing.Point(6, 56);
+            this.classText.Name = "classText";
+            this.classText.Size = new System.Drawing.Size(285, 38);
+            this.classText.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.checkedListBox1);
+            this.groupBox2.Location = new System.Drawing.Point(78, 182);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(421, 410);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Existing Classes";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(409, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Remove Checked";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.removeClassBtn);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 50);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(409, 284);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.addFileBtn);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.fileListBox);
+            this.groupBox3.Location = new System.Drawing.Point(600, 33);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(402, 559);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Course Datasheets";
+            // 
+            // addFileBtn
+            // 
+            this.addFileBtn.Location = new System.Drawing.Point(6, 41);
+            this.addFileBtn.Name = "addFileBtn";
+            this.addFileBtn.Size = new System.Drawing.Size(169, 53);
+            this.addFileBtn.TabIndex = 3;
+            this.addFileBtn.Text = "Add File";
+            this.addFileBtn.UseVisualStyleBackColor = true;
+            this.addFileBtn.Click += new System.EventHandler(this.addFileBtn_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 490);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(390, 60);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Remove Selected";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // fileListBox
+            // 
+            this.fileListBox.FormattingEnabled = true;
+            this.fileListBox.ItemHeight = 31;
+            this.fileListBox.Location = new System.Drawing.Point(6, 108);
+            this.fileListBox.Name = "fileListBox";
+            this.fileListBox.Size = new System.Drawing.Size(390, 376);
+            this.fileListBox.TabIndex = 0;
+            this.fileListBox.SelectedIndexChanged += new System.EventHandler(this.fileListBox_SelectedIndexChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // CommonChipPins
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 694);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CommonChipPins";
+            this.Text = "CommonChipPins";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button addClassBtn;
+        private System.Windows.Forms.TextBox classText;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox fileListBox;
+        private System.Windows.Forms.Button addFileBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
 }
--- ece-calculator-bg4/ece-calculator-bg4/CommonChipPins.cs
+++ ece-calculator-bg4/ece-calculator-bg4/CommonChipPins.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,160 @@ namespace ece_calculator_bg4
 		public CommonChipPins()
 		{
 			InitializeComponent();
-		}
-	}
+            addFileBtn.Enabled = false;
+
+            loadClassListBox();     // Display/Load All Class Files
+            System.Diagnostics.Debug.WriteLine("Initialized Correctly");
+        }
+
+        private void addNewClassBtn(object sender, EventArgs e)
+        {
+            if (classText.Text != "")
+            {
+                string newPath = Path.GetDirectoryName(Application.ExecutablePath) + "\\Resources\\" + classText.Text;
+
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                    loadClassListBox(); //  
+                }
+
+            }
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Change Occured on Class List
+            loadDatasheetsListBox();            // Update the List of Data Sheets
+
+            if (checkedListBox1.SelectedItem == null)
+            {
+                addFileBtn.Enabled = false;
+            }
+            else
+            {
+                addFileBtn.Enabled = true;
+            }
+        }
+
+        private void loadClassListBox()
+        {
+            checkedListBox1.Items.Clear();
+            string resourcePath = Path.GetDirectoryName(Application.ExecutablePath) + "\\Resources";
+
+            foreach (var d in System.IO.Directory.GetDirectories(resourcePath))
+            {
+                var tempFolder = new DirectoryInfo(d);
+                var folderName = tempFolder.Name;
+                checkedListBox1.Items.Add(folderName);
+            }
+        }
+
+        private void loadDatasheetsListBox()
+        {
+            fileListBox.Items.Clear();
+            string selClass = checkedListBox1.SelectedItem.ToString();
+            string resourcePath = Path.GetDirectoryName(Application.ExecutablePath) + "\\Resources\\" + selClass;
+            System.Diagnostics.Debug.WriteLine(selClass);
+
+            DirectoryInfo dir = new DirectoryInfo(resourcePath); //Assuming Test is your Folder
+
+            FileInfo[] Files = dir.GetFiles();
+
+            foreach (FileInfo file in Files)
+            {
+                fileListBox.Items.Add(file.Name);
+            }
+        }
+        private void addFileButton(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PDF Files|*.pdf";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string sourcePath = openFileDialog.FileName;
+                string fileName = Path.GetFileName(sourcePath);
+
+                string selClass = checkedListBox1.SelectedItem.ToString();
+                string copyToPath = Path.GetDirectoryName(Application.ExecutablePath) + "\\Resources\\" + selClass + "\\" + fileName;
+                System.IO.File.Copy(sourcePath, copyToPath, true);
+                System.Diagnostics.Debug.WriteLine(sourcePath);
+                System.Diagnostics.Debug.WriteLine(fileName);
+
+                loadDatasheetsListBox();
+            }
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void fileListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Index Changed on Datasheet List
+            string filePath = Path.GetDirectoryName(Application.ExecutablePath) + "\\Resources\\" + checkedListBox1.SelectedItem + "\\" + fileListBox.SelectedItem;
+            System.Diagnostics.Debug.WriteLine(filePath);
+            System.Diagnostics.Process.Start(filePath);
+        }
+
+        private void removeClassBtn(object sender, EventArgs e)
+        {
+            // GOAL: Delete the checked Directories/Files
+            // 1. Loop Through each directory
+            // 2. Remove Each Element Inside Directory
+            // 3. Delete the Empty Directory
+            // 4. Repeat
+
+            var checkedItems = checkedListBox1.CheckedItems;
+            for (var i = 0; i < checkedItems.Count; i++)
+            {
+                // Get Directory
+                var tempPathName = Path.GetDirectoryName(Application.ExecutablePath) + "\\Resources\\" + checkedItems[i];
+                System.Diagnostics.Debug.WriteLine(tempPathName);
+                System.IO.DirectoryInfo di = new DirectoryInfo(tempPathName);
+
+                // 2. Remove Each Element Inside Directory
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                // 3. Delete Empty Directory/File
+                Directory.Delete(tempPathName);
+            }
+            loadClassListBox();
+            fileListBox.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PDF Files|*.pdf";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string sourcePath = openFileDialog.FileName;
+                string fileName = Path.GetFileName(sourcePath);
+
+                string selClass = checkedListBox1.SelectedItem.ToString();
+                string copyToPath = Path.GetDirectoryName(Application.ExecutablePath) + "\\Resources\\" + selClass + "\\" + fileName;
+                System.IO.File.Copy(sourcePath, copyToPath, true);
+                System.Diagnostics.Debug.WriteLine(sourcePath);
+                System.Diagnostics.Debug.WriteLine(fileName);
+
+                loadDatasheetsListBox();
+            }
+
+        }
+
+
+    }
 }
