46d8fa5ef07666f62ec47a2f73e0bf2b351a0452
josiahdeleon <josiah993@gmail.com>
2019-11-13 19:30:28 -0800

    Color changes
    Ok for real this one should work. All the color stuff

--- ece-calculator-bg4/ece-calculator-bg4/ResistorColor.Designer.cs
+++ ece-calculator-bg4/ece-calculator-bg4/ResistorColor.Designer.cs
@@ -39,6 +39,8 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // value0
@@ -55,9 +57,10 @@
             "Violet",
             "Grey",
             "White"});
-            this.value0.Location = new System.Drawing.Point(148, 224);
+            this.value0.Location = new System.Drawing.Point(197, 276);
+            this.value0.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.value0.Name = "value0";
-            this.value0.Size = new System.Drawing.Size(121, 21);
+            this.value0.Size = new System.Drawing.Size(160, 24);
             this.value0.TabIndex = 0;
             this.value0.SelectedIndexChanged += new System.EventHandler(this.value0_changed);
             // 
@@ -75,9 +78,10 @@
             "Violet",
             "Grey",
             "White"});
-            this.value1.Location = new System.Drawing.Point(275, 224);
+            this.value1.Location = new System.Drawing.Point(367, 276);
+            this.value1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.value1.Name = "value1";
-            this.value1.Size = new System.Drawing.Size(121, 21);
+            this.value1.Size = new System.Drawing.Size(160, 24);
             this.value1.TabIndex = 1;
             this.value1.SelectedIndexChanged += new System.EventHandler(this.value1_changed);
             // 
@@ -95,9 +99,10 @@
             "Violet",
             "Grey",
             "White"});
-            this.multiplier.Location = new System.Drawing.Point(402, 224);
+            this.multiplier.Location = new System.Drawing.Point(536, 276);
+            this.multiplier.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.multiplier.Name = "multiplier";
-            this.multiplier.Size = new System.Drawing.Size(113, 21);
+            this.multiplier.Size = new System.Drawing.Size(149, 24);
             this.multiplier.TabIndex = 2;
             this.multiplier.SelectedIndexChanged += new System.EventHandler(this.multiplier_changed);
             // 
@@ -107,78 +112,110 @@
             this.tolerance.Items.AddRange(new object[] {
             "Gold",
             "Silver"});
-            this.tolerance.Location = new System.Drawing.Point(521, 224);
+            this.tolerance.Location = new System.Drawing.Point(695, 276);
+            this.tolerance.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tolerance.Name = "tolerance";
-            this.tolerance.Size = new System.Drawing.Size(121, 21);
+            this.tolerance.Size = new System.Drawing.Size(160, 24);
             this.tolerance.TabIndex = 3;
             this.tolerance.SelectedIndexChanged += new System.EventHandler(this.tolerance_SelectedIndexChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(340, 302);
+            this.textBox1.Location = new System.Drawing.Point(453, 372);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(197, 208);
+            this.label1.Location = new System.Drawing.Point(263, 256);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.Size = new System.Drawing.Size(53, 17);
             this.label1.TabIndex = 6;
             this.label1.Text = "Band 1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(316, 208);
+            this.label2.Location = new System.Drawing.Point(421, 256);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.Size = new System.Drawing.Size(53, 17);
             this.label2.TabIndex = 7;
             this.label2.Text = "Band 2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(436, 208);
+            this.label3.Location = new System.Drawing.Point(581, 256);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.Size = new System.Drawing.Size(64, 17);
             this.label3.TabIndex = 8;
             this.label3.Text = "Multiplier";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(553, 208);
+            this.label4.Location = new System.Drawing.Point(737, 256);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.Size = new System.Drawing.Size(72, 17);
             this.label4.TabIndex = 9;
             this.label4.Text = "Tolerance";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(361, 286);
+            this.label5.Location = new System.Drawing.Point(481, 352);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.Size = new System.Drawing.Size(78, 17);
             this.label5.TabIndex = 10;
             this.label5.Text = "Resistance";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(446, 305);
+            this.label6.Location = new System.Drawing.Point(595, 375);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(46, 17);
             this.label6.TabIndex = 11;
             this.label6.Text = "label6";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "None",
+            "Brown",
+            "Blue"});
+            this.comboBox1.Location = new System.Drawing.Point(55, 276);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(66, 256);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Resistor Color";
+            // 
             // ResistorColor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -190,6 +227,7 @@
             this.Controls.Add(this.multiplier);
             this.Controls.Add(this.value1);
             this.Controls.Add(this.value0);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ResistorColor";
             this.Text = "ResistorColor";
             this.ResumeLayout(false);
@@ -210,5 +248,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-	}
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+    }
 }
