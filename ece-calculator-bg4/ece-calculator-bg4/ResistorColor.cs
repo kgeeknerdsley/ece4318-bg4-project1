db36f0b00fd8316bf948b6b2dc49c2aea8aa1069
Keane1996 <57118441+Keane1996@users.noreply.github.com>
2019-11-13 19:57:48 -0800

    Add files via upload

--- ece-calculator-bg4/ece-calculator-bg4/ResistorColor.cs
+++ ece-calculator-bg4/ece-calculator-bg4/ResistorColor.cs
@@ -19,10 +19,10 @@ namespace ece_calculator_bg4
             value1.SelectedIndex = 0;
             multiplier.SelectedIndex = 0;
             tolerance.SelectedIndex = 0;
+            comboBox1.SelectedIndex = 0;
             textBox1.TextAlign = HorizontalAlignment.Right;
 
         }
-        int i = 0;
         int temp_resistance = 0;
         double resistance = 0;
         double power = 0;
@@ -37,50 +37,53 @@ namespace ece_calculator_bg4
 
             Graphics myGraphics0 = base.CreateGraphics();
             SolidBrush mySolidBrush0 = new SolidBrush(Color.Black);
-            switch (value0.SelectedIndex)
+            if(comboBox1.SelectedIndex != 0)
             {
-                case 0:
-                    mySolidBrush0 = new SolidBrush(Color.Black);
-                    myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
-                    break;
-                case 1:
-                    mySolidBrush0 = new SolidBrush(Color.Brown);
-                    myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
-                    break;
-                case 2:
-                    mySolidBrush0 = new SolidBrush(Color.Red);
-                    myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
-                    break;
-                case 3:
-                    mySolidBrush0 = new SolidBrush(Color.Orange);
-                    myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
-                    break;
-                case 4:
-                    mySolidBrush0 = new SolidBrush(Color.Yellow);
-                    myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
-                    break;
-                case 5:
-                    mySolidBrush0 = new SolidBrush(Color.Green);
-                    myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
-                    break;
-                case 6:
-                    mySolidBrush0 = new SolidBrush(Color.Blue);
-                    myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
-                    break;
-                case 7:
-                    mySolidBrush0 = new SolidBrush(Color.Violet);
-                    myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
-                    break;
-                case 8:
-                    mySolidBrush0 = new SolidBrush(Color.Gray);
-                    myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
-                    break;
-                case 9:
-                    mySolidBrush0 = new SolidBrush(Color.White);
-                    myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
-                    break;
-
+                switch (value0.SelectedIndex)
+                {
+                    case 0:
+                        mySolidBrush0 = new SolidBrush(Color.Black);
+                        myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
+                        break;
+                    case 1:
+                        mySolidBrush0 = new SolidBrush(Color.Brown);
+                        myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
+                        break;
+                    case 2:
+                        mySolidBrush0 = new SolidBrush(Color.Red);
+                        myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
+                        break;
+                    case 3:
+                        mySolidBrush0 = new SolidBrush(Color.Orange);
+                        myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
+                        break;
+                    case 4:
+                        mySolidBrush0 = new SolidBrush(Color.Yellow);
+                        myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
+                        break;
+                    case 5:
+                        mySolidBrush0 = new SolidBrush(Color.Green);
+                        myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
+                        break;
+                    case 6:
+                        mySolidBrush0 = new SolidBrush(Color.Blue);
+                        myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
+                        break;
+                    case 7:
+                        mySolidBrush0 = new SolidBrush(Color.Violet);
+                        myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
+                        break;
+                    case 8:
+                        mySolidBrush0 = new SolidBrush(Color.Gray);
+                        myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
+                        break;
+                    case 9:
+                        mySolidBrush0 = new SolidBrush(Color.White);
+                        myGraphics0.FillRectangle(mySolidBrush0, 50, 50, 50, 150);
+                        break;
+                }
             }
+            
         }
         private void value1_changed(object sender, EventArgs e)
         {
@@ -91,50 +94,53 @@ namespace ece_calculator_bg4
             final_value(sender, e);
             Graphics myGraphics1 = base.CreateGraphics();
             SolidBrush mySolidBrush1 = new SolidBrush(Color.Black);
-            switch (value1.SelectedIndex)
+            if(comboBox1.SelectedIndex != 0)
             {
-                case 0:
-                    mySolidBrush1 = new SolidBrush(Color.Black);
-                    myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
-                    break;
-                case 1:
-                    mySolidBrush1 = new SolidBrush(Color.Brown);
-                    myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
-                    break;
-                case 2:
-                    mySolidBrush1 = new SolidBrush(Color.Red);
-                    myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
-                    break;
-                case 3:
-                    mySolidBrush1 = new SolidBrush(Color.Orange);
-                    myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
-                    break;
-                case 4:
-                    mySolidBrush1 = new SolidBrush(Color.Yellow);
-                    myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
-                    break;
-                case 5:
-                    mySolidBrush1 = new SolidBrush(Color.Green);
-                    myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
-                    break;
-                case 6:
-                    mySolidBrush1 = new SolidBrush(Color.Blue);
-                    myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
-                    break;
-                case 7:
-                    mySolidBrush1 = new SolidBrush(Color.Violet);
-                    myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
-                    break;
-                case 8:
-                    mySolidBrush1 = new SolidBrush(Color.Gray);
-                    myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
-                    break;
-                case 9:
-                    mySolidBrush1 = new SolidBrush(Color.White);
-                    myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
-                    break;
-
+                switch (value1.SelectedIndex)
+                {
+                    case 0:
+                        mySolidBrush1 = new SolidBrush(Color.Black);
+                        myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
+                        break;
+                    case 1:
+                        mySolidBrush1 = new SolidBrush(Color.Brown);
+                        myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
+                        break;
+                    case 2:
+                        mySolidBrush1 = new SolidBrush(Color.Red);
+                        myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
+                        break;
+                    case 3:
+                        mySolidBrush1 = new SolidBrush(Color.Orange);
+                        myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
+                        break;
+                    case 4:
+                        mySolidBrush1 = new SolidBrush(Color.Yellow);
+                        myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
+                        break;
+                    case 5:
+                        mySolidBrush1 = new SolidBrush(Color.Green);
+                        myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
+                        break;
+                    case 6:
+                        mySolidBrush1 = new SolidBrush(Color.Blue);
+                        myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
+                        break;
+                    case 7:
+                        mySolidBrush1 = new SolidBrush(Color.Violet);
+                        myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
+                        break;
+                    case 8:
+                        mySolidBrush1 = new SolidBrush(Color.Gray);
+                        myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
+                        break;
+                    case 9:
+                        mySolidBrush1 = new SolidBrush(Color.White);
+                        myGraphics1.FillRectangle(mySolidBrush1, 150, 50, 50, 150);
+                        break;
+                }
             }
+            
         }
 
         private void multiplier_changed(object sender, EventArgs e)
@@ -143,69 +149,76 @@ namespace ece_calculator_bg4
             final_value(sender, e);
             Graphics myGraphics2 = base.CreateGraphics();
             SolidBrush mySolidBrush2 = new SolidBrush(Color.Black);
-            switch (multiplier.SelectedIndex)
+            if(comboBox1.SelectedIndex != 0)
             {
-                case 0:
-                    mySolidBrush2 = new SolidBrush(Color.Black);
-                    myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
-                    break;
-                case 1:
-                    mySolidBrush2 = new SolidBrush(Color.Brown);
-                    myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
-                    break;
-                case 2:
-                    mySolidBrush2 = new SolidBrush(Color.Red);
-                    myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
-                    break;
-                case 3:
-                    mySolidBrush2 = new SolidBrush(Color.Orange);
-                    myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
-                    break;
-                case 4:
-                    mySolidBrush2 = new SolidBrush(Color.Yellow);
-                    myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
-                    break;
-                case 5:
-                    mySolidBrush2 = new SolidBrush(Color.Green);
-                    myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
-                    break;
-                case 6:
-                    mySolidBrush2 = new SolidBrush(Color.Blue);
-                    myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
-                    break;
-                case 7:
-                    mySolidBrush2 = new SolidBrush(Color.Violet);
-                    myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
-                    break;
-                case 8:
-                    mySolidBrush2 = new SolidBrush(Color.Gray);
-                    myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
-                    break;
-                case 9:
-                    mySolidBrush2 = new SolidBrush(Color.White);
-                    myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
-                    break;
-
+                switch (multiplier.SelectedIndex)
+                {
+                    case 0:
+                        mySolidBrush2 = new SolidBrush(Color.Black);
+                        myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
+                        break;
+                    case 1:
+                        mySolidBrush2 = new SolidBrush(Color.Brown);
+                        myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
+                        break;
+                    case 2:
+                        mySolidBrush2 = new SolidBrush(Color.Red);
+                        myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
+                        break;
+                    case 3:
+                        mySolidBrush2 = new SolidBrush(Color.Orange);
+                        myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
+                        break;
+                    case 4:
+                        mySolidBrush2 = new SolidBrush(Color.Yellow);
+                        myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
+                        break;
+                    case 5:
+                        mySolidBrush2 = new SolidBrush(Color.Green);
+                        myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
+                        break;
+                    case 6:
+                        mySolidBrush2 = new SolidBrush(Color.Blue);
+                        myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
+                        break;
+                    case 7:
+                        mySolidBrush2 = new SolidBrush(Color.Violet);
+                        myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
+                        break;
+                    case 8:
+                        mySolidBrush2 = new SolidBrush(Color.Gray);
+                        myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
+                        break;
+                    case 9:
+                        mySolidBrush2 = new SolidBrush(Color.White);
+                        myGraphics2.FillRectangle(mySolidBrush2, 250, 50, 50, 150);
+                        break;
+                }
             }
+            
         }
 
         private void tolerance_SelectedIndexChanged(object sender, EventArgs e)
         {
             Graphics myGraphics3 = base.CreateGraphics();
             SolidBrush mySolidBrush3 = new SolidBrush(Color.Black);
-            switch (tolerance.SelectedIndex)
+            if (comboBox1.SelectedIndex != 0)
             {
-                case 0:
-                    toler = "5%";
-                    mySolidBrush3 = new SolidBrush(Color.Gold);
-                    myGraphics3.FillRectangle(mySolidBrush3, 350, 50, 50, 150);
-                    break;
-                case 1:
-                    toler = "10%";
-                    mySolidBrush3 = new SolidBrush(Color.Silver);
-                    myGraphics3.FillRectangle(mySolidBrush3, 350, 50, 50, 150);
-                    break;
+                switch (tolerance.SelectedIndex)
+                {
+                    case 0:
+                        toler = "5%";
+                        mySolidBrush3 = new SolidBrush(Color.Gold);
+                        myGraphics3.FillRectangle(mySolidBrush3, 350, 50, 50, 150);
+                        break;
+                    case 1:
+                        toler = "10%";
+                        mySolidBrush3 = new SolidBrush(Color.Silver);
+                        myGraphics3.FillRectangle(mySolidBrush3, 350, 50, 50, 150);
+                        break;
+                }
             }
+            
             label6.Text = "+- " + toler;
 
         }
@@ -230,11 +243,6 @@ namespace ece_calculator_bg4
                 textBox1.Text = temp + " KΩ";
             else if (i == 0)
                 textBox1.Text = temp + " Ω";
-
-        }
-
-        private void ResistorColor_Load(object sender, EventArgs e)
-        {
 
         }
 
